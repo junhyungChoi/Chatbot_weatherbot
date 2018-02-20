335ecf68d8505794c22a99d9680b24dc1f6c9077
junhyungChoi <skyman0303@naver.com>
2018-02-21 00:05:26 +0900

    First! chatbot
    First! chatbot

---
+++ weatherbot/bothub/bot.py
@@ -0,0 +1,29 @@
+﻿# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+from bothub_client.bot import BaseBot
+from bothub_client.messages import Message
+
+from .weather import get_weather
+
+
+class Bot(BaseBot):
+
+    def handle_message(self, event, context):
+        location = event.get('location')
+        appid = '6a5beaeff30e04d92c42f59d7c17eba5'
+
+        if location:
+            lat = location['latitude']
+            lon = location['longitude']
+            weather = get_weather(lat, lon, appid)
+            self.send_message(weather)
+
+        msg = Message(event).set_text('날씨 확인을 위해 현재 위치를 전송해주세요.') \
+                            .add_location_request('Send Location')
+        self.send_message(msg)
+		
+		# msg = Message(event).set_text('날씨 확인을 위해 현재 위치를 전송해주세요.')
+        # self.send_message('날씨 확인을 위해 현재 위치를 전송해주세요.')
+		# self.send_message(msg)
---
+++ weatherbot/bothub/weather.py
@@ -0,0 +1,26 @@
+﻿# -*- coding: utf-8 -*-
+
+import requests
+
+base_url = 'http://api.openweathermap.org/data/2.5/weather'
+
+def get_weather(lat, lon, appid):
+    url = '{}?lat={}&lon={}&appid={}'.format(base_url, lat, lon, appid)
+    response = requests.get(url)
+    data = response.json()
+    country = data.get('sys')['country']
+    name = data.get('name')
+    temp = float(data.get('main')['temp']) - 273.15
+    wind = data.get('wind')['speed']
+    cloud = data.get('weather')[0]['description']
+    humidity = data.get('main')['humidity']
+    
+    message = '''
+Country: {}, {}
+Temp: {} °C
+Wind: {} m/s
+Cloudiness: {}
+Humidity: {} %
+'''.format(name, country, round(temp, 1), wind, cloud, humidity)
+    
+    return message
